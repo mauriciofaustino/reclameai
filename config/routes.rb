293b36b2124723cb531f38511e948e88a8c301eb
Mauricio Castro <mauricio.faustino@gmail.com>
2019-09-20 16:17:35 -0300

    adicionado css :art:

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :complaints, only: [:new, :create, :show, :index]
+  root to: redirect('/complaints/new')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
